ddba312bc5e07193a43fe0fcb62afaa2056adfe2
rakafebriansy <kerenr445@gmail.com>
2024-07-22 08:25:29 +0700

    feat: styling input file components

--- src/components/Elements/InputFile.jsx
+++ src/components/Elements/InputFile.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
 import { UploadFileIcon } from "../../functions/svgs";
+import { useRef } from "react";
 
-const InputFile = () => {
+const InputFile = (props) => {
+    const {name} = props;
+    const [files, setFiles] = useState(null);
+    const inputRef = useRef(null);
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    };
+
+    const handleDrop = (e) => {
+        e.preventDefault();
+        setFiles(e.dataTransfer.files);
+    };
+
+
     return (
-        <div className="flex items-center justify-center w-full">
-            <label for="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+        <div onDragOver={handleDragOver} onDrop={handleDrop} className="flex items-center justify-center w-full input-file">
+            <label htmlFor="dropzone-file" className={`flex flex-col items-center justify-center w-full h-64 border-2 ${files && files.length > 0 ? 'border-primary' : 'border-gray-300'} border-dashed rounded-lg cursor-pointer hover:border-primary ${files && files.length > 0 ? 'bg-gray-100' : 'bg-gray-50'} hover:bg-gray-100`}>
                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <UploadFileIcon/>
-                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                    <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                    <UploadFileIcon strokeColor={files && files.length > 0 ? 'stroke-primary' : 'stroke-[#9B9B9B]'} />
+                    {(!files || files.length == 0) && (
+                        <>
+                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                        </>
+                    )}
+                    {files && files.length > 0 && (
+                        <>
+                        <p className="mb-2 text-sm text-primary dark:text-gray-400">{files[0].name}</p>
+                        </>
+                    )}
                 </div>
-                <input id="dropzone-file" type="file" className="hidden" />
+                <input id="dropzone-file" type="file" className="hidden" onChange={e => {setFiles(e.target.files)}} ref={inputRef}/>
             </label>
         </div> 
     );
